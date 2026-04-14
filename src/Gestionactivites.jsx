1e983286836f776f40e3812ada78cfec085ebb7c
Djeryldev <djeryl42@gmail.com>
2026-04-14 11:02:50 +0100

    mise a jour

--- src/Gestionactivites.jsx
+++ src/Gestionactivites.jsx
@@ -1,67 +1,63 @@
 import { act, useState } from "react";
 
-function Gestionactivites(){
-    const [activites, setactivites]= useState([
-        {id: 1, nom:"programmation" , terminee: false},
-        {id: 2, nom:"robotique" , terminee: false},
-        {id: 3, nom:"art" , terminee: false},
-        {id: 4, nom:"design" , terminee: false}
-    ]);
+function Gestionactivites (){
 
+    const [activites, setactivites] = useState([
+        {id: 1, nom: "programmation", terminee: false},
+        {id: 2, nom: "robotique", terminee: false},
+        {id: 3, nom: "art", terminee: false},
+        {id: 4, nom: "design", terminee: false}
+    ]);
     const [nouvelleactivite, setnouvelleactivite] = useState('');
 
-function ajouteractivite(){
-    const nouvelid = activites.length +1;
-    if(nouvelleactivite.trim() !== ''){
-        setactivites([...activites, {
-            id : nouvelid, nom: nouvelleactivite, terminee : false
-        }]);
+    function ajouteractivite(){
+        const nouvelleid = activites.length +1;
+        setactivites([...activites, 
+            {id: nouvelleid, nom: nouvelleactivite, terminee:false}
+        ]);
         setnouvelleactivite('');
     }
 
-}
-function supprimeractiviter(idsuprimer){
-    const nouvellesactivites =  activites.filter(activite => activite.id !== idsuprimer);
-    setactivites(nouvellesactivites);
-}
-
-function termineractiviter(idaterminee){
-    const nouvellesactivites = activites.map(activite => {
-        if( activite.id == idaterminee){
-            return {...activite, terminee: !activite.terminee};
-        }
-        return activite;
-    })
-    setactivites(nouvellesactivites);
-
-}
+    function supprimeractivite(idasupprimer){
+        const nouvellesactivites = activites.filter(activite => activite.id !== idasupprimer);
+        setactivites(nouvellesactivites);
+    }
+     
+    function termineractivite(idaterminer){
+        const nouvellesactivites = activites.map(activite => {
+            if( activite.id === idaterminer){
+                //modifier le statut
+                return {...activite, terminee: !activite.terminee};
+            }
+            return activite;
+        });
+        setactivites(nouvellesactivites);
+    }
 
     return(
-
         <div>
-            <h1> mes activites</h1>
+            <h1>mes activites</h1>
+
             <div>
                 <input type="text"
-                placeholder="nouvelle activite" 
-                value={nouvelleactivite}
+                placeholder="nouvelle activite"
+                value={nouvelleactivite} 
                 onChange={(e) => setnouvelleactivite(e.target.value)}/> <br />
                 <button onClick={ajouteractivite}>ajouter</button>
             </div>
-            <div>
-                <ul>
-                    {activites.map((activite) => (
-                        <li key={activite.id} style={{ 
-                            textDecoration: activite.terminee ? 'line-through' : 'none',
+
+            <ul>
+                {activites.map((activite) =>(
+                    <li key={activite.id} style={{
+                        textDecoration: activite.terminee ? 'line-through' : 'none',
                             color: activite.terminee ? 'gray' : 'black'
-                        }}>{activite.nom} <br />
-                        <button onClick={() => termineractiviter(activite.id)}>{activite.terminee ? 'annuler' : 'terminee' }</button><br />
-                        <button onClick={() => supprimeractiviter(activite.id)}>supprimer</button>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+                    }} >{activite.nom} <br />
+                     <button onClick={() => termineractivite(activite.id)}>{ activite.terminee ? 'annuler':'terminer'}</button>
+                     <button onClick={() => supprimeractivite(activite.id)}>supprimer</button>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
-
 }
 export default Gestionactivites;
